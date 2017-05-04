fca22a0f3c0ee04437f5450079f9c4f86f6ba9f6
ludovic <none>
2017-05-04 19:46:22 +0200

    comment ouvrier

--- src/com/company/Ouvrier.java
+++ src/com/company/Ouvrier.java
@@ -31,6 +31,7 @@ public class Ouvrier extends Thread
                     {
                     ViderBenne();}
                     Thread.sleep((long) Math.ceil(Math.random() * 100));
+                    System.out.println("l'ouvrier met le bois dans la chaine de montage");
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
